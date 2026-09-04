93f9a129da3d666334db46c4d99bf48731fad736
Aman Ali <sayyedamanali1340@gmail.com>
2026-09-05 00:09:40 +0530

    Add files via upload

---
+++ UNIT-3/SET-6/Question-24.py
@@ -0,0 +1,10 @@
+# Organise a program using functions to:
+# Accept student name and marks
+# Calculate total
+# Calculate average
+# Determine grade
+# Display result
+
+# Condition: main() should control the complete program, while each individual task
+# should be performed by a separate function.
+
